36d95eb6a4a5769e496f3503a5b2961bbb6334de
Mullayam <mullayam06@gmail.com>
2025-08-16 03:16:15 +0530

    fix the build

--- src/components/common/framer-tabs.tsx
+++ src/components/common/framer-tabs.tsx
@@ -87,7 +87,7 @@ export default function FramingTabs() {
               {tabs.map((tab, index) => (
                 <div
                   key={index}
-                  ref={(el) => (tabRefs.current[index] = el)}
+                  ref={(el:any) => (tabRefs.current[index] = el )}
                   className={`px-3 py-2 cursor-pointer transition-colors duration-300 h-[30px] ${
                     index === activeIndex ? "text-[#0e0e10] dark:text-white" : "text-[#0e0f1199] dark:text-[#ffffff99]"
                   }`}
