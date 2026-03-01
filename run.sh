3793000546766242f3e79a1a3b956d1e20129ba7
Mullayam singh <mullayam06@gmail.com>
2026-03-01 09:33:14 +0530

    build script fix

--- run.sh
+++ run.sh
@@ -5,9 +5,7 @@ set -e
 
 echo "🚀 Starting deployment..."
 
-# Navigate to your project directory (optional)
-cd /home/mullayam/projects/email-service/airsend-mail-client || exit 1
-
+ 
 echo "📥 Pulling latest changes from Git..."
 git pull origin main
 
